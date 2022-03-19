3537bffb2371fb7ba0d472df91c528b9c6e60169
Jeff Montinard <68262228+jmontinard@users.noreply.github.com>
2022-03-18 22:58:12 -0400

    add in Js code
    added in dark mode html,css,js modified premade code for this app

--- index.js
+++ index.js
@@ -142,33 +142,52 @@ colorSchemeBtn.addEventListener('click', ()=>{
 
 })
 
-// @media screen and(prefers-color-scheme: light){
 
-// }
+//Dark mode 
 
-// @media screen and (prefers-color-scheme: light) {
-//     body {  
-//       background-color: white;
-//       color: black;  
-//     }
-      
-//     .dark-mode {
-//       background-color: black;
-//       color: white;
-//     }
-//   }
-//   .smooth-transition {
-//     transition: background-color 300ms;
-//   }
-
-//   @media screen and (prefers-color-scheme: dark) {
-//     .dark-mode {
-//       background-color: white;
-//       color: black;
-//     }
-    
-//     body {  
-//       background-color: black;
-//       color: white;  
-//     }
-//   }
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
+const getStyle = (element, style) => 
+  window
+    .getComputedStyle(element)
+    .getPropertyValue(style);
+const initialColors = {
+  bg: getStyle(html, "--bg"),
+}
+const darkMode = {
+  bg: "#333333", // override styles here
+}
+const transformKey = key => 
+  "--" + key.replace(/([A-Z])/, "-$1").toLowerCase();
+const changeColors = (colors) => {
+  Object.keys(colors).map(key => 
+    html.style.setProperty(transformKey(key), colors[key]) 
+  );
+}
+checkbox.addEventListener("change", ({target}) => {
+    target.checked ? changeColors(darkMode) : changeColors(initialColors);
+});
+const isExistLocalStorage = (key) => 
+  localStorage.getItem(key) != null;
+const createOrEditLocalStorage = (key, value) => 
+  localStorage.setItem(key, JSON.stringify(value));
+const getValeuLocalStorage = (key) =>
+  JSON.parse(localStorage.getItem(key));
+checkbox.addEventListener("change", ({target}) => {
+  if (target.checked) {
+    changeColors(darkMode);
+    createOrEditLocalStorage('mode','darkMode');
+  } else {
+    changeColors(initialColors);
+    createOrEditLocalStorage('mode','initialColors');
+  }
+})
+if(!isExistLocalStorage('mode'))
+  createOrEditLocalStorage('mode', 'initialColors');
+if (getValeuLocalStorage('mode') === "initialColors") {
+  checkbox.removeAttribute('checked');
+  changeColors(initialColors);
+} else {
+  checkbox.setAttribute('checked', "");
+  changeColors(darkMode);
+}
