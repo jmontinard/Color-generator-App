c21cdb6934193332e4c4b572a7e5bb1d2f89683c
Jeff Montinard <68262228+jmontinard@users.noreply.github.com>
2022-04-13 09:23:39 -0400

    Final updates

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 
 const seedColor = document.querySelector('#seedColor')
-
 const colorSchemeBtn = document.querySelector('#colorSchemeBtn')
-// const colorScheme = document.querySelector('#colorScheme').value
 const colorSchemeID = document.querySelector('#colorScheme')
 const color1 = document.querySelector('#color1')
 const color2 = document.querySelector('#color2')
 const color3 = document.querySelector('#color3')
 const color4 = document.querySelector('#color4')
 const color5 = document.querySelector('#color5')
-
-// const colorArray = Array.from(document.querySelectorAll('.color'))
 const colorArray = document.querySelectorAll('.color')
-
-// console.log(colorArray)
-// add in copy code
 const colorTextArr = document.querySelectorAll('.hex')
-
 
 const colorText1 = document.querySelector('#hex-text-1')
 const colorText2 = document.querySelector('#hex-text-2')
 const colorText3 = document.querySelector('#hex-text-3')
 const colorText4 = document.querySelector('#hex-text-4')
 const colorText5 = document.querySelector('#hex-text-5')
-
-
 
 const defaultState = () =>{
     let URL = `https://www.thecolorapi.com/scheme?hex=0047AB&mode=monochrome`
@@ -42,30 +32,16 @@ const defaultState = () =>{
     colorText3.textContent = `${data.colors[2].hex.value}`
     colorText4.textContent = `${data.colors[3].hex.value}`
     colorText5.textContent = `${data.colors[4].hex.value}`
-    
-    
-        console.log(data) 
-    
-    
     })
     .catch(err => console.log(err))
-
-
 }
-
 
 const copyColor = () =>{
     document.execCommand("copy");
   }
 
-
 const copyColorLogic = () =>{
-   
-
-    // console.log(colorTextArr)
-    
     colorArray.forEach((color,index) =>{
-       
         color.addEventListener("copy", (e) => {
           e.preventDefault();
           if (event.clipboardData) {
@@ -86,15 +62,11 @@ colorTextArr.forEach(hexText =>{
           console.log(e.clipboardData.getData("text"))
         }
       });
-})
-}
+})}
 
 copyColorLogic()
 copyHexLogic()
-
-
 // mouse and touch events hex code to apear on color block 
-
 const colorHexHoverLogic = () =>{
     colorArray.forEach((color, index) =>{
         color.addEventListener('mouseover', ()=> color.textContent = colorTextArr[index].textContent)
@@ -103,13 +75,7 @@ const colorHexHoverLogic = () =>{
     })}
 
 //  add in touch logic 
-
 colorHexHoverLogic()
-
-// add in dark mode 
-
-
-
 
 colorSchemeBtn.addEventListener('click', ()=>{
     const hexColor = seedColor.value.substring(1)
@@ -131,48 +97,32 @@ colorSchemeBtn.addEventListener('click', ()=>{
     colorText3.textContent = `${data.colors[2].hex.value}`
     colorText4.textContent = `${data.colors[3].hex.value}`
     colorText5.textContent = `${data.colors[4].hex.value}`
-    
-    
-        console.log(data) 
-    
-    
     })
     .catch(err => console.log(err))
-
-
 })
 
-
 //Dark mode 
-
 const html = document.querySelector("html")
 const checkbox = document.querySelector("input[name=theme]")
-const getStyle = (element, style) => 
-  window
-    .getComputedStyle(element)
-    .getPropertyValue(style);
-const initialColors = {
-//   bg: getStyle(html, "--bg"),
-  bg: '#FCFCFC'
+// const getStyle = (element, style) => 
+//   window
+//     .getComputedStyle(element)
+//     .getPropertyValue(style);
+// const initialColors = {
+// //   bg: getStyle(html, "--bg"),
+//   bg: '#FCFCFC'
   
-}
-const darkMode = {
-  bg: "#1f2937", // override styles here
-  colorChange: () => {
-      colorSchemeID.style.backgroundColor = "#1f2937";
-      colorSchemeID.style.color = "#FCFCFC" 
-      colorSchemeBtn.style.backgroundColor = "#1f2937"; 
-      colorSchemeBtn.style.color = "#FCFCFC";
-      colorTextArr.forEach(hex => hex.style.color = "#FCFCFC")
-  }
-  
-//   get this working 
-// this is something to work on 
-// this tooo jj jsjsjs hvjfjyf
-//hgjhgjh add last touches kaska ny
-// kanye west akslnkansalksdskldskkdlfkddlkwkwlekwklklklzaksk
-}
-
+// }
+// const darkMode = {
+//   bg: "#1f2937", // override styles here
+//   colorChange: () => {
+//       colorSchemeID.style.backgroundColor = "#1f2937";
+//       colorSchemeID.style.color = "#FCFCFC" 
+//       colorSchemeBtn.style.backgroundColor = "#1f2937"; 
+//       colorSchemeBtn.style.color = "#FCFCFC";
+//       colorTextArr.forEach(hex => hex.style.color = "#FCFCFC")
+//   }
+// }
 
 
 const colorChangeDark = () => {
@@ -193,15 +143,14 @@ const colorChangeLight = () => {
     document.body.style.backgroundColor = '#FCFCFC'
 }
 
-// console.log(colorChange())
 
-const transformKey = key => 
-  "--" + key.replace(/([A-Z])/, "-$1").toLowerCase();
-const changeColors = (colors) => {
-  Object.keys(colors).map(key => 
-    html.style.setProperty(transformKey(key), colors[key]) 
-  );
-}
+// const transformKey = key => 
+//   "--" + key.replace(/([A-Z])/, "-$1").toLowerCase();
+// const changeColors = (colors) => {
+//   Object.keys(colors).map(key => 
+//     html.style.setProperty(transformKey(key), colors[key]) 
+//   );
+// }
 
 const darkModeSwitcherLogic = () =>{
     const colorScheme = document.querySelector('#colorScheme')
@@ -225,24 +174,12 @@ const createOrEditLocalStorage = (key, value) =>
 const getValeuLocalStorage = (key) =>
   JSON.parse(localStorage.getItem(key));
 checkbox.addEventListener("change", ({target}) => {
-
-// darkModeSwitcherLogic(target)
   if (target.checked) {
-  
-   
-   
     createOrEditLocalStorage('mode','darkMode');
   } else {
-    // changeColors(initialColors);
-   
-
     createOrEditLocalStorage('mode','lightMode');
   }
 })
-
-
-
-
 
 if(!isExistLocalStorage('mode'))
   createOrEditLocalStorage('mode', 'lightMode');
